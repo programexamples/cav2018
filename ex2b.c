7e44fb0b5ec6e31e66d300c0add702f71b92b242
Program examples <example@example.com>
2018-01-12 15:43:10 +0100

    adding ex2b.c

---
+++ ex2b.c
@@ -0,0 +1,33 @@
+
+/* Summary:
+ *
+ *  %ret = a0 and %ret = a and b = b0 and b0 <= 0
+ *  %ret = a+4b and %ret = a+4b0 and a = a0 and %ret >= a+4
+ *
+ */
+int ex2b(int a, int b)
+{
+    int x = a;
+    int y = b;
+    int i;
+    
+    for (i=0; i < y; i++) 
+    {
+        x = x + 2;
+    }
+
+    for (i=0; i < y; i++) 
+    {
+        x = x + 2;
+    }
+
+    return x;
+}
+
+void test()
+{
+    int a;
+    int b;
+
+    int r = ex2b(a, b);
+}
