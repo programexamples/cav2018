2a0b8783008bec711327ad6131b8468011e233e4
Program examples <example@example.com>
2018-01-12 15:51:09 +0100

    adding mul2.c

---
+++ mul2.c
@@ -0,0 +1,27 @@
+
+/* Summary:
+ *
+ * %ret = a0 and %ret = a and %ret <= 0
+ * %ret = 2a0 and %ret = 2a and %ret >= 2
+ *
+ */
+int mul2(int a)
+{
+    int x = a;
+    int i;
+
+    for(i = 0; i < a; i++)
+    {
+        x = x+1;
+    }
+
+    return x;
+}
+
+void test()
+{
+    int r;
+    int a;
+
+    r = mul2(a);
+}
