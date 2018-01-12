53118a6c5f1bfcd00bf10c4be5e7d66757290676
Program examples <example@example.com>
2018-01-12 15:53:24 +0100

    adding abs.c

---
+++ abs.c
@@ -0,0 +1,27 @@
+
+/* Summary:
+ *
+ * %ret = x0 and %ret = x and %ret >= 0
+ * %ret = -x0 and %ret = -x and %ret >= 1
+ *
+ */
+int my_abs(int x)
+{
+    int r;
+
+    if(x < 0)
+        r = -x;
+    else
+        r = x;
+
+    return r;
+}
+
+void test()
+{
+    int r;
+    int k = 42;
+
+    r = my_abs(k);
+}
+
