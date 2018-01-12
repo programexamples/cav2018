d7695d6eae8530c6274f9e406bd6da8885ae929a
Program examples <example@example.com>
2018-01-12 15:17:23 +0100

    add computed_th.c

---
+++ computed_th.c
@@ -0,0 +1,46 @@
+extern int undet();
+
+/* Summary:
+ *
+ * r = a and r = a0 and n0 = 0 and n = 0 and r >= 0
+ * r = a and r = a0 and n = n0 and n >= 1 and r >= n
+ * r = n0 and r = n and a = a0 and a0 >= 0 and r >= a0+1
+ * r = r0 and n = n0 and a = a0 and a0 <= -1 and n >= 0
+ * r = r0 and n = n0 and a = a0 and n <= -1 
+ *
+ */
+void computed_th(int * r, int n, int a){
+    int i;
+    int j;
+    int th;
+
+    if(n < 0 || a < 0)
+        return;
+
+    th = 0;
+    i = 0;
+
+    while(i < n){
+        th++;
+        i++;
+    }
+    
+    j = a;
+    while(j < th){
+        j++;
+    }
+
+    *r = j;
+}
+
+void test()
+{
+    int r;
+    int n;
+    int a;
+
+    n = undet();
+    a = undet();
+
+    computed_th(&r, n, a);
+}
