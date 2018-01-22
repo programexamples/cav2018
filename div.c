6b9d5a91f131f0f87260f6725ef8b5d18ac806f6
Program examples <example@example.com>
2018-01-22 14:29:49 +0100

    adding div.c

---
+++ div.c
@@ -0,0 +1,59 @@
+
+/* Summary:
+
+[ 2b >= a and a >= b+1 ]
+a = a0 and r = a-b and b = b0 and q = 1 and 2b >= a and a >= b+1
+
+or
+
+[ b >= 1 and a >= 2b+1 ]
+a = a0 and b = b0 and r >= 1 and q >= 1 and q+r >= 3 and b >= r
+and a+1 >= 2b+q and a >= 2b+1
+
+or
+
+[ b >= a and b >= 1 and a >= 0 ]
+a = a0 and r = a and b = b0 and q = 0 and b >= a and b >= 1 and a >= 0
+
+or
+
+[ a >= 0 and b <= 0 ]
+a = a0 and b = b0 and q = q0 and r = r0 and b <= 0 and a >= 0
+
+or
+
+[ a <= -1 ]
+a = a0 and b = b0 and q = q0 and r = r0 and a <= -1
+
+*/
+void div(int a, int b, int * q, int * r)
+{
+    if(a < 0)
+        return;
+
+    if(b < 1)
+        return;
+
+    *q = 0;
+    *r = a;
+
+    if(*r > b)
+    {
+        do
+        {
+            *r = *r - b;
+            *q = *q + 1;
+        }
+        while(*r > b);
+    }
+}
+
+void test()
+{
+    int a = 6;
+    int b = 2;
+    int q;
+    int r;
+
+    div(a, b, &q, &r);
+}
